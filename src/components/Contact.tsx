10d00b2f2d2d8564db6357231eb214dc71e44b92
Jaydeep Patel <jaydeepatel2001@gmail.com>
2026-03-15 11:25:03 +0530

    updt: removed console.logs()

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -31,9 +31,6 @@ const Contact: React.FC = () => {
     try {
       // NOTE: You need to replace these with your actual EmailJS credentials
       // Sign up at https://www.emailjs.com/
-      console.log(">>>>>>>>", import.meta.env.VITE_EMAILJS_SERVICE_ID)
-      console.log(">>>>>>>>", import.meta.env.VITE_EMAILJS_TEMPLATE_ID)
-      console.log(">>>>>>>>", import.meta.env.VITE_EMAILJS_PUBLIC_KEY)
 
       await emailjs.send(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
